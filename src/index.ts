769605b8b3d9b5bd89b1f7d2d53fab8bbe4a7860
roman105 <36539654+roman105@users.noreply.github.com>
2021-08-30 15:28:57 +0800

    initial commit

---
+++ src/index.ts
@@ -0,0 +1,7 @@
+/*
+ * Public API Surface of @youwol/flux-deform
+ */
+export * from './lib/main';
+export * from './lib/deform.module';
+export * from './lib/deform';
+export * from './lib/skinParameters';
---
+++ src/lib/deform.ts
@@ -0,0 +1,58 @@
+import { BufferAttribute, BufferGeometry } from "three"
+import { Serie } from "@youwol/dataframe"
+import { SkinParameters } from './skinParameters'
+
+/**
+ * @see [[deform]]
+ * @category Skin Parameters
+ */
+export class DeformParameters extends SkinParameters {
+    public readonly scaleX: number
+    public readonly scaleY: number
+    public readonly scaleZ: number
+
+    constructor(
+        {scaleX=1, scaleY=1, scaleZ=1, ...others}:
+        {scaleX?: number, scaleY?: number, scaleZ?: number}={})
+    {
+        super(others)
+        this.scaleX = scaleX || 1
+        this.scaleY = scaleY || 1
+        this.scaleZ = scaleZ || 1
+    }
+}
+
+/**
+ * @example
+ * ```ts
+ * const s = createSurface( df.series['positions'].array, df.series['indices'].array )
+ * const newGeom = deform( s.geometry, df.series['U'].array )
+ * ```
+ * @category Skins
+ */
+export function deform(
+    {geometry, deformVector, parameters}:
+    {geometry: BufferGeometry, deformVector: Serie, parameters?: DeformParameters}): Serie
+{
+    if (geometry === undefined) throw new Error('geometry is undefined')
+    const position = geometry.getAttribute('position') as BufferAttribute
+
+    if (deformVector.count !== position.count) throw new Error('attribute should have 3 x nb vertices')
+
+    //const geom = position.clone()
+    return deformVector.map( (v,i) => [
+        position.getX(i) + parameters.scaleX*v[0],
+        position.getY(i) + parameters.scaleX*v[1],
+        position.getZ(i) + parameters.scaleX*v[2]
+    ])
+
+    // for (let i = 0; i < position.count; ++i) {
+    //     geom.setXYZ(i,
+    //         position.getX(i) + parameters.scaleX*attribute[3*i],
+    //         position.getY(i) + parameters.scaleY*attribute[3*i+1],
+    //         position.getZ(i) + parameters.scaleZ*attribute[3*i+2]
+    //     )
+    // }
+
+    // return geom
+}
